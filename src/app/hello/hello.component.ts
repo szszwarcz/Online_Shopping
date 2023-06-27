21a7b7158ecab16049064a30a5051c4205c65734
Szymon.Szwarczynski <szymon.szwarczynski@gmail.com>
2023-06-27 12:12:21 +0200

    Added basic information display

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./hello.component.css']
 })
 export class HelloComponent {
-  courseName:string = "Angular";
-  constructor(){};
-  ngOnInit(){};
+  userData = {
+    userName: "John Doe",
+    emailId: "john.doe@gmail.com",
+    contactNo: 9876543210,
+    address: "424 McVaney Road, Franklin, North Carolina, 28734",
+    height: "5'6",
+    weight: 82,
+    education: "High school diploma",
+  }
 }
