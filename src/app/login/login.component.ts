5496e08cb4ffad03147762b4ef704cbb0ec3cd16
Szymon.Szwarczynski <szymon.szwarczynski@gmail.com>
2023-06-28 15:33:50 +0200

    Added 2way comm into login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,23 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  applyClass= true;
-  imgUrl:string = "assets/Landscape_walpaper.jpg";
-  userName = "Jajc";
-  userPassword = "ek";
+  users : Login[] = [];
+  userName = "";
+  userPassword = "";
   clickme(user:string,password:string){
+    let loginTemp = new Login(user,password);
+    this.users.push(loginTemp);
+  }
+}
+
+class Login{
+  userName:string;
+  userPassword:string;
+  constructor(user:string,password:string) {
     this.userName=user;
     this.userPassword=password;
-  }
-  checkAge(ageChecker:number){
-    ageChecker>=21 ? "You are an adult" : ageChecker>=13? "You are a teen ager" : "You are a kid";
-  }
-  styleObj1 = {
-    backgroundColor:'orange',
-    color : 'black'
-  }
-  styleObj2 = {
-    backgroundColor:'greenyellow',
-    color : 'white'
   }
 }
