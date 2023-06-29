b1e8b87a30de662711d03fcd806821e2c269eae9
Szymon.Szwarczynski <szymon.szwarczynski@gmail.com>
2023-06-29 14:24:26 +0200

    Learnt directives

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,9 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  isBordered : boolean = true;
+  numero : number = 0;
   users : Login[] = [];
   userName = "";
   userPassword = "";
+  loginSuccess = false;
+  loginFailed = false;
+  coursers:string[] = ["Angular", "React","JavaScript"]
+  increment(){
+    if(this.numero != 6){
+      this.numero++;
+    }
+    else{
+      this.numero = 1;
+    }
+    
+  }
+  Login2(){
+    this.loginFailed = false;
+    this.loginSuccess=false;
+    if(this.userName=="admin" && this.userPassword=="admin"){
+      this.loginSuccess = true;
+    }
+    else{
+      this.loginFailed=true;
+    }
+  }
+
   clickme(user:string,password:string){
     let loginTemp = new Login(user,password);
     this.users.push(loginTemp);
