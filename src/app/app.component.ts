973df7060a78adad544cb5ad1a6a1866a3d0cd73
Szymon Szwarczynski <szymon.szwarczynski@gmail.com>
2023-07-06 15:59:19 +0200

    Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name :string = "Angular";
+  data!: Observable<number>;
+  myArray : number[] = [];
+  errors!: boolean;
+  finished!: boolean;
+
+  fetchData(){
+    this.finished=false;
+    this.data = new Observable(observer =>{
+      setTimeout(() => {observer.next(11);},1000),
+      setTimeout(() => {observer.next(12);},2000),
+      setTimeout(() => {observer.complete();},3000)
+      });
+
+    let sub = this.data.subscribe((value) => this.myArray.push(value),error => this.errors =true, ()=> this.finished = true);
+  }
+
+  myObservable:Observable<number> = of(1,2,3);
+  myObservable2:Observable<number[]> = of([1,2,3],[4,5,6],[7,8,9]);
+
+  display(){
+    this.myObservable.subscribe(data=>console.log(data));
+    this.myObservable2.subscribe(data=>console.log(data));
+  }
 }
